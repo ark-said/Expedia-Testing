aa53e898c38697949da14b84515315c7a7bcc98e
Ali Said Ark <arkalisaid@gmail.com>
2022-09-16 16:09:34 +0300

    git17

--- src/test/java/pages/ExpediaPages.java
+++ src/test/java/pages/ExpediaPages.java
@@ -235,9 +235,4 @@ public WebElement OneWayLeavingFromSearchForIstanbul;
 
     @FindBy (xpath = "//div[@class='uitk-text uitk-type-200 uitk-text-default-theme']")
     public WebElement SigninPageKeepSignedInButtonText;
-
-
-
-
-
 }
